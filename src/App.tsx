007a3a44ae7589d180a570fb747a65c9cf39a354
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2025-07-03 00:30:35 +0530

    feat(app): Add app title on top of calculator UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import styles from './app.module.scss';
 export function App() {
   return (
     <div className={styles.app}>
+      <h2 style={{color: 'var(--primary-color)'}}>
+        Neomorphcalc
+      </h2>
       <CalculatorUi />
     </div>
   );
