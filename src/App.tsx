7e4df46a8b2d03bd7986a07f0ab3a2841c3198f7
Shubham Vishwakarma <90132246+shubham-kv@users.noreply.github.com>
2025-07-03 11:54:59 +0530

    fix(app): Add some meta tags for SEO

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
+import { Helmet } from 'react-helmet-async';
 import { CalculatorUi } from '@/components';
+import { generateMetadata } from './metadata';
 import styles from './app.module.css';
+
+const meta = generateMetadata({ title: 'Home' });
 
 export function App() {
   return (
-    <div className={styles.app}>
-      <h2 style={{color: 'var(--primary-color)'}}>
-        Neomorphcalc
-      </h2>
-      <CalculatorUi />
-    </div>
+    <>
+      <Helmet titleTemplate={meta.titleTemplate}>
+        <title>{meta.title}</title>
+        <meta name='description' content={meta.description} />
+        <meta name='author' content={meta.author} />
+        <meta name='application-name' content={meta.applicationName} />
+        <meta property='og:title' content={meta.openGraph.title} />
+        <meta property='og:description' content={meta.openGraph.description} />
+        <meta property='og:type' content={meta.openGraph.type} />
+        <meta property='og:url' content={meta.openGraph.url} />
+        <meta property='og:site_name' content={meta.openGraph.siteName} />
+      </Helmet>
+
+      <div className={styles.app}>
+        <h2 style={{ color: 'var(--primary-color)' }}>Neomorphcalc</h2>
+        <CalculatorUi />
+      </div>
+    </>
   );
 }
